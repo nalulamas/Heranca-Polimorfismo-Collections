7036cfd6dfa28b11274d9bc3f8e1e0876f865fca
nalulamas <67602155+nalulamas@users.noreply.github.com>
2022-02-02 15:51:21 -0300

    Update Ex2Principal.java

--- Ex2Principal.java
+++ Ex2Principal.java
@@ -1,4 +1,4 @@
-package HerançaEx2;
+package HeranÃ§aEx2;
 
 public class Ex2Principal {
 
@@ -7,23 +7,30 @@ public class Ex2Principal {
 		CavaloEx2 pocoto = new CavaloEx2();
 		PreguicaEx2 lazy = new PreguicaEx2();
 		
-		System.out.println(" Especificações do cachorro");
+		System.out.println(" EspecificaÃ§Ãµes do cachorro");
 		auau.setNome("Bruno");
 		auau.setIdade(2);
 		auau.emitirSom();
 		auau.locomocao();
+		System.out.println(auau.getNome());
+		System.out.println(auau.getIdade());
 		
-		System.out.println(" Especificações do Cavalo");
+		System.out.println(" EspecificaÃ§Ãµes do Cavalo");
 		pocoto.setNome("Junin");
 		pocoto.setIdade(7);
 		pocoto.emitirSom();
 		pocoto.locomocao();
+		System.out.println(pocoto.getNome());
+		System.out.println(pocoto.getIdade());
 		
-		System.out.println(" Especificações da Preguiça");
+		System.out.println(" EspecificaÃ§Ãµes da PreguiÃ§a");
 		lazy.setNome("Jully");
 		lazy.setIdade(4);
 		lazy.emitirSom();
 		lazy.locomocao();
+		System.out.println(lazy.getIdade());
+		System.out.println(lazy.getNome());
 	}
 
 }
+
